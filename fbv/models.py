f9c728878e388b1ea318c1f9cb3278ff56c6c6b5
Sisi <dqgsh1055@gmail.com>
2019-10-31 20:50:10 +0900

    시현_git 연동

---
+++ fbv/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+
+    # 했는지 여부
+    checked = models.BooleanField(
+        default = False
+    )
+    # 작성시간
+    created_at = models.DateTimeField(
+        auto_now_add = True
+    )
+    # 수정시간
+    modified_at = models.DateTimeField(
+        auto_now = True
+    )
+    # 제목
+    title = models.CharField(
+        max_length = 150
+    )
+    # 마감기한
+    # 사용: date(1960, 8, 1)
+    due_date = models.DateField()
+    
